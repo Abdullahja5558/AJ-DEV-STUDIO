46aeac0bc777b4a978e67b26cd58298c4b3cd0fd
Abdullah.dev <abdullahjaved5558@gmail.com>
2026-05-15 19:53:36 +0500

    fix ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -92,7 +92,7 @@ export const Navbar = () => {
 
   return (
     <>
-      {/* SEO/Accessibility Fix: Added aria controls and label to the button */}
+      
       <div className="fixed top-6 right-6 md:top-10 md:right-12 z-[300]">
         <button 
           onClick={() => setIsOpen(!isOpen)}
